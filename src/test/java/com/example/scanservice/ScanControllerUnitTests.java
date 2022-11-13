8ece05c26feea82144265e7eede31390ae8f4c22
johnny urkens <58312023+johnny-urkens@users.noreply.github.com>
2022-11-13 23:30:35 +0100

    lowercase

--- src/test/java/com/example/scanservice/ScanControllerUnitTests.java
+++ src/test/java/com/example/scanservice/ScanControllerUnitTests.java
@@ -34,24 +34,24 @@ class ScanControllerUnitTests {
 
     @Test
     void givenScan_whenGetScansByUserName_thenReturnJsonScans() throws Exception {
-        Scan scanUser1Car1 = new Scan("Lode","Audi A4",5);
+        Scan scanUser1Car1 = new Scan("Lode","Audi",5);
         Scan scanUser1Car2 = new Scan("Lode","Tesla",3);
 
         List<Scan> scanList = new ArrayList<>();
         scanList.add(scanUser1Car1);
         scanList.add(scanUser1Car2);
 
-        given(scanRepository.findScansByUserName("Lode")).willReturn(scanList);
+        given(scanRepository.findScansByUserName("lode")).willReturn(scanList);
 
-        mockMvc.perform(get("/scans/user/{userName}","Lode"))
+        mockMvc.perform(get("/scans/user/{userName}","lode"))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$", hasSize(2)))
-            .andExpect(jsonPath("$[0].userName",is("Lode")))
-            .andExpect(jsonPath("$[0].carBrand",is("Audi A4")))
+            .andExpect(jsonPath("$[0].userName",is("lode")))
+            .andExpect(jsonPath("$[0].carBrand",is("audi")))
             .andExpect(jsonPath("$[0].scoreNumber",is(5)))
-            .andExpect(jsonPath("$[1].userName",is("Lode")))
-            .andExpect(jsonPath("$[1].carBrand",is("Tesla")))
+            .andExpect(jsonPath("$[1].userName",is("lode")))
+            .andExpect(jsonPath("$[1].carBrand",is("tesla")))
             .andExpect(jsonPath("$[1].scoreNumber",is(3)));
     }
 
@@ -64,17 +64,17 @@ class ScanControllerUnitTests {
         scanList.add(scanUser1Car1);
         scanList.add(scanUser1Car2);
 
-        given(scanRepository.findScansByCarBrand("Tesla")).willReturn(scanList);
+        given(scanRepository.findScansByCarBrand("tesla")).willReturn(scanList);
 
-        mockMvc.perform(get("/scans/{carBrand}","Tesla"))
+        mockMvc.perform(get("/scans/{carBrand}","tesla"))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$", hasSize(2)))
-            .andExpect(jsonPath("$[0].userName",is("Lode")))
-            .andExpect(jsonPath("$[0].carBrand",is("Tesla")))
+            .andExpect(jsonPath("$[0].userName",is("lode")))
+            .andExpect(jsonPath("$[0].carBrand",is("tesla")))
             .andExpect(jsonPath("$[0].scoreNumber",is(5)))
-            .andExpect(jsonPath("$[1].userName",is("Johnny")))
-            .andExpect(jsonPath("$[1].carBrand",is("Tesla")))
+            .andExpect(jsonPath("$[1].userName",is("johnny")))
+            .andExpect(jsonPath("$[1].carBrand",is("tesla")))
             .andExpect(jsonPath("$[1].scoreNumber",is(3)));
     }
 
@@ -82,13 +82,13 @@ class ScanControllerUnitTests {
     void givenScan_whenGetScanByUserNameAndCarBrand_thenReturnJsonScan() throws Exception {
         Scan scanUser1Car1 = new Scan("Lode","Tesla",3);
 
-        given(scanRepository.findScanByUserNameAndAndCarBrand("Lode","Tesla")).willReturn(scanUser1Car1);
+        given(scanRepository.findScanByUserNameAndAndCarBrand("lode","tesla")).willReturn(scanUser1Car1);
 
-        mockMvc.perform(get("/scans/user/{userName}/car/{carBrand}","Lode","Tesla"))
+        mockMvc.perform(get("/scans/user/{userName}/car/{carBrand}","lode","tesla"))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.userName",is("Lode")))
-            .andExpect(jsonPath("$.carBrand",is("Tesla")))
+            .andExpect(jsonPath("$.userName",is("lode")))
+            .andExpect(jsonPath("$.carBrand",is("tesla")))
             .andExpect(jsonPath("$.scoreNumber",is(3)));
     }
 
@@ -107,11 +107,11 @@ class ScanControllerUnitTests {
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$", hasSize(2)))
-            .andExpect(jsonPath("$[0].userName",is("Lode")))
-            .andExpect(jsonPath("$[0].carBrand",is("Tesla")))
+            .andExpect(jsonPath("$[0].userName",is("lode")))
+            .andExpect(jsonPath("$[0].carBrand",is("tesla")))
             .andExpect(jsonPath("$[0].scoreNumber",is(5)))
-            .andExpect(jsonPath("$[1].userName",is("Johnny")))
-            .andExpect(jsonPath("$[1].carBrand",is("Tesla")))
+            .andExpect(jsonPath("$[1].userName",is("johnny")))
+            .andExpect(jsonPath("$[1].carBrand",is("tesla")))
             .andExpect(jsonPath("$[1].scoreNumber",is(3)));
     }
 
@@ -119,52 +119,52 @@ class ScanControllerUnitTests {
 
     @Test
     void whenPostScan_thenReturnJsonScan() throws Exception{
-        Scan ScanUser3Car1 = new Scan("Michael","Audi A4",4);
+        Scan ScanUser3Car1 = new Scan("Michael","Audi",4);
 
         mockMvc.perform(post("/scans")
                 .content(mapper.writeValueAsString(ScanUser3Car1))
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.userName",is("Michael")))
-            .andExpect(jsonPath("$.carBrand",is("Audi A4")))
+            .andExpect(jsonPath("$.userName",is("michael")))
+            .andExpect(jsonPath("$.carBrand",is("audi")))
             .andExpect(jsonPath("$.scoreNumber",is(4)));
     }
 
     @Test
     void givenScan_whenPutScan_thenReturnJsonScan() throws Exception{
-        Scan ScanUser3Car1 = new Scan("Michael","Audi A4",4);
+        Scan ScanUser3Car1 = new Scan("Michael","Audi",4);
 
-        given(scanRepository.findScanByUserNameAndAndCarBrand("Michael","Audi A4")).willReturn(ScanUser3Car1);
+        given(scanRepository.findScanByUserNameAndAndCarBrand("michael","audi")).willReturn(ScanUser3Car1);
 
-        Scan updatedReview = new Scan("Michael","Audi A4",5);
+        Scan updatedReview = new Scan("Michael","Audi",5);
 
         mockMvc.perform(put("/scans")
                 .content(mapper.writeValueAsString(updatedReview))
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.userName",is("Michael")))
-            .andExpect(jsonPath("$.carBrand",is("Audi A4")))
+            .andExpect(jsonPath("$.userName",is("michael")))
+            .andExpect(jsonPath("$.carBrand",is("audi")))
             .andExpect(jsonPath("$.scoreNumber",is(5)));
     }
 
     @Test
     void givenScan_whenDeleteScan_thenStatusOk() throws Exception{
-        Scan ScanUser3Car1 = new Scan("Michael","Audi A4",4);
+        Scan ScanUser3Car1 = new Scan("Michael","Audi",4);
 
-        given(scanRepository.findScanByUserNameAndAndCarBrand("Michael","Audi A4")).willReturn(ScanUser3Car1);
+        given(scanRepository.findScanByUserNameAndAndCarBrand("michael","audi")).willReturn(ScanUser3Car1);
 
-        mockMvc.perform(delete("/scans/user/{userName}/car/{carBrand}","Michael","Audi A4")
+        mockMvc.perform(delete("/scans/user/{userName}/car/{carBrand}","michael","audi")
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
     }
 
     @Test
     void givenNoScan_whenDeleteScan_thenStatusNotFound() throws Exception{
-        given(scanRepository.findScanByUserNameAndAndCarBrand("Lode","Volvo")).willReturn(null);
+        given(scanRepository.findScanByUserNameAndAndCarBrand("lode","volvo")).willReturn(null);
 
-        mockMvc.perform(delete("/scans/user/{userName}/car/{carBrand}","Lode","Volvo")
+        mockMvc.perform(delete("/scans/user/{userName}/car/{carBrand}","lode","volvo")
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isNotFound());
     }
@@ -174,20 +174,20 @@ class ScanControllerUnitTests {
     void givenNewScanIsPut_thenReturnJsonScan() throws Exception{
         Scan newScan = new Scan();
         newScan.setUserName("Michael");
-        newScan.setCarBrand("Audi A4");
+        newScan.setCarBrand("Audi");
         newScan.setScoreNumber(4);
 
-        given(scanRepository.findScanByUserNameAndAndCarBrand("Michael","Audi A4")).willReturn(newScan);
+        given(scanRepository.findScanByUserNameAndAndCarBrand("michael","audi")).willReturn(newScan);
 
-        Scan updatedReview = new Scan("Michael","Audi A4",5);
+        Scan updatedReview = new Scan("Michael","Audi",5);
 
         mockMvc.perform(put("/scans")
                 .content(mapper.writeValueAsString(updatedReview))
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.userName",is("Michael")))
-            .andExpect(jsonPath("$.carBrand",is("Audi A4")))
+            .andExpect(jsonPath("$.userName",is("michael")))
+            .andExpect(jsonPath("$.carBrand",is("audi")))
             .andExpect(jsonPath("$.scoreNumber",is(5)));
     }
 }
